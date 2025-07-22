07d265d22773605729902412b69472bb1f87bd44
lumi72 <luumi@tuta.io>
2025-07-22 18:30:06 +0800

    add steam

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,14 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+
   services.v2raya = {
     enable = true;
     cliPackage = pkgs.xray;
